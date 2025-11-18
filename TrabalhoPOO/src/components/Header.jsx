885eb94cf598765c68df98e9f00511f5f6c89fc4
Name <viniciusdelima2017@gmail.com>
2025-11-18 11:13:27 -0300

    Adiciona Logo

--- TrabalhoPOO/src/components/Header.jsx
+++ TrabalhoPOO/src/components/Header.jsx
@@ -4,8 +4,8 @@ import logoImagem from '../assets/logo.png';
 export default function Header() {
   return (
     <>
-    <div className = "bg-red-600 py-16" >
-        <img src={logoImagem}></img>
+    <div className = "bg-red-600 py-16 flex justify-center" >
+        <img src={logoImagem} className="h-25 p-0 border-spacing-0"></img>
     </div>
     
     </>
