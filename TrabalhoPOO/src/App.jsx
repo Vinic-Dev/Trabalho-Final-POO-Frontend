eee5da7b865e2ebbc849f2fd5562d4845661711d
Vini <viniciusdelima2017@gmail.com>
2025-11-26 16:14:46 -0300

    Correção na leitura do json

--- TrabalhoPOO/src/App.jsx
+++ TrabalhoPOO/src/App.jsx
@@ -1,19 +1,13 @@
 import Header from "./components/Header";
 import Nave from "./components/Navegador";
+import Corpo from "./components/Corpo";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <Header />
-      <Nave />
-      <main className="p-8">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">
-          Bem-vindo ao Sistema
-        </h1>
-        <p className="text-gray-600">
-          Este é um texto cinza
-        </p>
-      </main>
+        <Header />
+        <Nave />
+        <Corpo />
     </div>
   )
 }
--- TrabalhoPOO/src/components/Header.jsx
+++ TrabalhoPOO/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+
 import logoImagem from '../assets/logo.png';
 
 export default function Header() {
   return (
     <>
     <div className = "bg-red-600 py-16 flex justify-center" >
-        <img src={logoImagem} className="h-25 p-0 border-spacing-0"></img>
+        <img src={logoImagem} className="h-24 p-0 border-spacing-0"></img>
     </div>
     
     </>
